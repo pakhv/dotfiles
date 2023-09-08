f2072af093b75ae33c1b1e10cf317673b42daa77
vladislav.pakhteev <vladislav.pakhteev@rt.ru>
2023-09-08 20:18:04 +0000

    transparent background

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require("config.set")
 require("config.remap")
 require("config.lazy")
+require("config.background")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/config/background.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "None" })
