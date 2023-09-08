4032b72fd88fd9dd39794ee252f61819ce010a8b
vladislav.pakhteev <vladislav.pakhteev@rt.ru>
2023-09-08 17:53:26 +0000

    finaly moved nvim config from init.lua file

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,17 @@
+require("toggleterm").setup{
+  shell = 'bash',
+  direction = 'horizontal',
+  hidden = true,
+  auto_scroll = true,
+  start_in_insert = true,
+  open_mapping = [[<c-\>]]
+}
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", direction = 'float' })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
