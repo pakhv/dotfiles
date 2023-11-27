b5fb36f38d4ea73d728e4cb3dba48526af49a5c3
vladislav.pakhteev <vladislav.pakhteev@rt.ru>
2023-11-27 15:42:55 +0000

    remapped dap and added tmux neww in vim

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -19,9 +19,9 @@ dap.configurations.cs = {
 }
 
 vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+vim.keymap.set('n', '<F9>', dap.step_into, { desc = 'Debug: Step Into' })
+vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debug: Step Over' })
+vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debug: Step Out' })
 vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
 vim.keymap.set('n', '<leader>B', function()
   dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -2,3 +2,17 @@ vim.keymap.set('n', '<a-a>', require("harpoon.mark").add_file)
 vim.keymap.set('n', '<leader>ui', require("harpoon.ui").toggle_quick_menu)
 vim.keymap.set('n', '<a-,>', require("harpoon.ui").nav_prev)
 vim.keymap.set('n', '<a-.>', require("harpoon.ui").nav_next)
+
+
+vim.keymap.set('n', '<a-t>', function()
+  local last_tmux_window = vim.fn.system("tmux list-windows -F '#I' | tail -1")
+  local last_tmux_window_num = tonumber(last_tmux_window)
+
+  if (last_tmux_window_num == nil) then
+    print('Couldn\'t parse tmux window number')
+    return
+  end
+
+  local new_tmux_window = last_tmux_window_num + 1
+  require("harpoon.tmux").sendCommand(new_tmux_window, "fd\n")
+end)
