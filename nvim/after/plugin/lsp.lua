ff14bb8525f767b8201f30c2ef2348a70388b134
rust-enjoyer <pakhv420@gmail.com>
2024-05-29 19:48:24 +0000

    removed signature doc in insert mode

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -27,7 +27,7 @@ local on_attach = function(_, bufnr)
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation', buffer = bufnr })
+  vim.keymap.set({ 'n' }, '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation', buffer = bufnr })
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
