09309f123c2082c1ef5108570d57ca566e2de5d2
Пахтеев Владислав Игоревич <vladislav.pakhteev@rt.ru>
2025-05-08 13:52:46 +0000

    harpoon keymaps

--- nvim/lua/config/plugins/harpoon.lua
+++ nvim/lua/config/plugins/harpoon.lua
@@ -9,9 +9,14 @@ return {
       harpoon:setup()
 
       vim.keymap.set('n', '<a-a>', function() harpoon:list():add() end)
-      vim.keymap.set('n', '<leader>ui', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set('n', '<a-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
       vim.keymap.set('n', '<a-,>', function() harpoon:list():prev() end)
       vim.keymap.set('n', '<a-.>', function() harpoon:list():next() end)
+
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
     end
   }
 }
