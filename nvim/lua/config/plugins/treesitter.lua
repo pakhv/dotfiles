122f318b35e3fb69595968b43fbf5de7807e1fa8
pakhv <vladislav.pakhteev@rt.ru>
2025-07-26 18:12:10 +0300

    treesitter config

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -8,10 +8,17 @@ return {
 
         ensure_installed = { 'lua', 'c_sharp', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
 
-        auto_install = true,
+        auto_install = false,
 
         highlight = { enable = true, additional_vim_regex_highlighting = false },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            node_incremental = "]s",
+            node_decremental = "[s",
+          },
+        },
       }
     end
   }
