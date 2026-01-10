a6833eec4688b32bd2ce517e44258e84dee73b9b
pakhv <vladislav.pakhteev@rt.ru>
2026-01-10 21:56:00 +0300

    nvim config refactor

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -14,3 +14,37 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   group = format_group,
   pattern = '*',
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my-lsp-attach', { clear = true }),
+  callback = function(event)
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = 'LSP: ' .. desc
+      end
+
+      vim.keymap.set('n', keys, func,
+        { buffer = event.buf, desc = desc })
+    end
+    local border = 'rounded'
+
+    nmap('<F2>', vim.lsp.buf.rename, 'Rename')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('<leader>r', vim.lsp.buf.references,
+      'Add lsp [R]eferences to quickfix list')
+    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('gr', require('telescope.builtin').lsp_references,
+      '[G]oto [R]eferences')
+
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols,
+      '[D]ocument [S]ymbols')
+
+    nmap('K', function() vim.lsp.buf.hover { border = border } end,
+      'Hover Documentation')
+    vim.keymap.set({ 'i' }, '<C-s>',
+      function() vim.lsp.buf.signature_help { border = border } end,
+      { desc = 'Signature Documentation', buffer = event.buf })
+  end
+})
--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -13,44 +13,7 @@ return {
           }
         },
         { 'j-hui/fidget.nvim', opts = {} },
-        'saghen/blink.cmp',
       },
-      config = function()
-        local border = 'rounded'
-
-        vim.api.nvim_create_autocmd('LspAttach', {
-          group = vim.api.nvim_create_augroup('my-lsp-attach', { clear = true }),
-          callback = function(event)
-            local nmap = function(keys, func, desc)
-              if desc then
-                desc = 'LSP: ' .. desc
-              end
-
-              vim.keymap.set('n', keys, func,
-                { buffer = event.buf, desc = desc })
-            end
-
-            nmap('<F2>', vim.lsp.buf.rename, 'Rename')
-            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-            nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-            nmap('<leader>r', vim.lsp.buf.references,
-              'Add lsp [R]eferences to quickfix list')
-            nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-            nmap('gr', require('telescope.builtin').lsp_references,
-              '[G]oto [R]eferences')
-
-            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols,
-              '[D]ocument [S]ymbols')
-
-            nmap('K', function() vim.lsp.buf.hover { border = border } end,
-              'Hover Documentation')
-            vim.keymap.set({ 'i' }, '<C-s>',
-              function() vim.lsp.buf.signature_help { border = border } end,
-              { desc = 'Signature Documentation', buffer = event.buf })
-          end
-        })
-      end
     },
     {
       'folke/lazydev.nvim',
