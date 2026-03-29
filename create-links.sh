416b99b559ddb56b665be455f0810affcf0a86f7
pakhv <email>
2026-03-29 09:58:18 +0300

    create-links paths fix

--- create-links.sh
+++ create-links.sh
@@ -1,57 +1,57 @@
 #!/usr/bin/bash
 
-home_path="/home/pakhv/"
+home_path="/home/pakhv"
 
 if [[ ! -z $1 ]]; then
     home_path=$1
 fi
 
+home_path=$(realpath $home_path)
+
 # .zshrc
 echo 'symlink for .zshrc'
-ln -sf "${home_path}dotfiles/zsh/.zshrc" "${home_path}.zshrc"
+ln -sf "${home_path}/dotfiles/zsh/.zshrc" "${home_path}/.zshrc"
 
 # .tmux.conf
 echo 'symlink for .tmux.conf'
-ln -sf "${home_path}dotfiles/tmux/.tmux.conf" "${home_path}.tmux.conf"
+ln -sf "${home_path}/dotfiles/tmux/.tmux.conf" "${home_path}/.tmux.conf"
 
 # autosuggestions and highlights
-mkdir -p "${home_path}.zsh"
+mkdir -p "${home_path}/.zsh"
 
-if [[ ! -d "${home_path}.zsh/zsh-autosuggestions" ]]; then
+if [[ ! -d "${home_path}/.zsh/zsh-autosuggestions" ]]; then
     echo 'cloning zsh-autosuggestions'
-    git clone https://github.com/zsh-users/zsh-autosuggestions "${home_path}.zsh/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${home_path}/.zsh/zsh-autosuggestions"
 fi
 
-if [[ ! -d "${home_path}.zsh/zsh-syntax-highlighting" ]]; then
+if [[ ! -d "${home_path}/.zsh/zsh-syntax-highlighting" ]]; then
     echo 'cloning zsh-syntx-highlighting'
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${home_path}.zsh/zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${home_path}/.zsh/zsh-syntax-highlighting"
 fi
 
 # scripts
 echo 'symlink for scripts'
-mkdir -p "${home_path}.local"
-ln -sf "${home_path}dotfiles/scripts" "${home_path}.local"
+mkdir -p "${home_path}/.local"
+ln -sf "${home_path}/dotfiles/scripts" "${home_path}/.local"
 
 # i3wm
 echo 'symlink for i3'
-mkdir -p "${home_path}.config/i3"
-ln -sf "${home_path}dotfiles/i3/config" "${home_path}.config/i3/config"
+mkdir -p "${home_path}/.config/i3"
+ln -sf "${home_path}/dotfiles/i3/config" "${home_path}/.config/i3/config"
 
 # tmux catppuccin
-mkdir -p "${home_path}tmux/catppuccin"
+mkdir -p "${home_path}/tmux/catppuccin"
 
-if [[ ! -d "${home_path}tmux/catppuccin/tmux" ]]; then
+if [[ ! -d "${home_path}/tmux/catppuccin/tmux" ]]; then
     echo 'cloning tmux catppuccin theme'
-    git clone https://github.com/catppuccin/tmux.git "${home_path}tmux/catppuccin/tmux"
+    git clone https://github.com/catppuccin/tmux.git "${home_path}/tmux/catppuccin/tmux"
 fi
 
 # nvim config
 echo 'symlink for nvim config'
-ln -sf "${home_path}dotfiles/nvim" "${home_path}.config"
+ln -sf "${home_path}/dotfiles/nvim" "${home_path}/.config"
 
 # alacritty
-mkdir -p "${home_path}.config/alacritty"
+mkdir -p "${home_path}/.config/alacritty"
 echo 'symlink for alacritty'
-ln -sf "${home_path}dotfiles/alacritty/alacritty.toml" "${home_path}.config/alacritty/alacritty.toml"
-
-#install zsh, tmux, fzf, ripgrep
+ln -sf "${home_path}/dotfiles/alacritty/alacritty.toml" "${home_path}/.config/alacritty/alacritty.toml"
