8d5303548fc3df9df5694aa86e8381ca8c554c47
pakhv <vladislav.pakhteev@rt.ru>
2026-04-24 20:44:01 +0300

    nvim autocmd

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -6,25 +6,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
-local format_group = vim.api.nvim_create_augroup('FormatOnSave', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePre', {
   callback = function()
     vim.lsp.buf.format()
   end,
-  group = format_group,
+  group = vim.api.nvim_create_augroup('FormatOnSave', { clear = true }),
   pattern = '*',
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my-lsp-attach', { clear = true }),
-  callback = function(event)
+  callback = function(ev)
     local nmap = function(keys, func, desc)
       if desc then
         desc = 'LSP: ' .. desc
       end
 
       vim.keymap.set('n', keys, func,
-        { buffer = event.buf, desc = desc })
+        { buffer = ev.buf, desc = desc })
     end
 
     nmap('<F2>', vim.lsp.buf.rename, '[R]ename')
@@ -34,19 +33,36 @@ vim.api.nvim_create_autocmd('LspAttach', {
     nmap('<leader>r', vim.lsp.buf.references,
       'Add lsp [R]eferences to quickfix list')
     nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client == nil then
+      return;
+    end
+
+    if client.server_capabilities.documentHighlightProvider then
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = ev.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+      vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+        buffer = ev.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
   end
 })
 
-local pack_hooks = function(ev)
-  local name, kind = ev.data.spec.name, ev.data.kind
 
-  if name == 'LuaSnip' and (kind == 'install' or kind == 'update') then
-    vim.system({ 'make', 'install_jsregexp' }, { cwd = ev.data.path })
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+
+    if name == 'LuaSnip' and (kind == 'install' or kind == 'update') then
+      vim.system({ 'make', 'install_jsregexp' }, { cwd = ev.data.path })
+    end
+
+    if name == 'nvim-treesitter/nvim-treesitter' and (kind == 'install' or kind == 'update') then
+      vim.cmd('TSUpdate')
+    end
   end
-
-  if name == 'nvim-treesitter/nvim-treesitter' and (kind == 'install' or kind == 'update') then
-    vim.cmd('TSUpdate')
-  end
-end
-
-vim.api.nvim_create_autocmd('PackChanged', { callback = pack_hooks })
+})
